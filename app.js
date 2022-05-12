47578bae8ede69f34eaa76f807ede8d83805f223
gaspar-costa-calvo <gaspar.c.calvo@gmail.com>
2022-05-12 19:20:09 -0300

    reestructurando mercado liebre

---
+++ app.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log("Corriendo" + PORT);
+});
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+    //funcion controladora o handler
+      res.sendFile(path.join(__dirname, "views/home.html"))
+})
